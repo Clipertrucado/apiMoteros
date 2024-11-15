3e116ad0db8e958f7c65cabf2b74a72831c3a3fb
Clipertrucado <145342712+Clipertrucado@users.noreply.github.com>
2024-11-15 12:12:39 +0100

    Add files via upload

--- apiMoteros/src/main/java/JSIA/apiMoteros/servicios/ClubController.java
+++ apiMoteros/src/main/java/JSIA/apiMoteros/servicios/ClubController.java
@@ -1,6 +1,8 @@
 package JSIA.apiMoteros.servicios;
 
 import JSIA.apiMoteros.daos.*;
+import JSIA.apiMoteros.servicios.ClubService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
